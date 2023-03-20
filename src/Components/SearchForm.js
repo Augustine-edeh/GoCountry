3c5d58bdb8f55c05261dbae19aa31dc32e34292b
Augustine-edeh <info.augustinesedeh@gmail.com>
2023-03-21 00:01:19 +0100

    CountryValue State: Created countryValue state and implemented its management

--- src/Components/SearchForm.js
+++ src/Components/SearchForm.js
@@ -1,14 +1,28 @@
+import { useState } from "react";
 import "./SearchForm.css";
 
 const SearchForm = () => {
+  const [countryValue, setcountryValue] = useState("");
+  const changeHandler = (event) => {
+    setcountryValue(event.target.value);
+  };
+  const submitHandler = (event) => {
+    event.preventDefault();
+    // Adding 2-way binding for countryValue
+    setcountryValue("");
+  };
   return (
-    <form className="Form">
+    <form className="Form" onSubmit={submitHandler}>
       <input
         type="text"
         className="Form-input"
         placeholder="Enter country name"
+        value={countryValue}
+        onChange={changeHandler}
       />
-      <button className="Form-button">Search</button>
+      <button className="Form-button" type="submit">
+        Search
+      </button>
     </form>
   );
 };
