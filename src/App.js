20566fc7465a5878db46cdca4f4b685aa190a587
Augustine-edeh <info.augustinesedeh@gmail.com>
2023-03-21 23:28:14 +0100

    Added a Header Component: Closes #4

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Header from "./Components/Header";
 import Title from "./Components/Title";
 import SearchForm from "./Components/SearchForm";
 import Footer from "./Components/Footer";
@@ -6,6 +7,7 @@ import Footer from "./Components/Footer";
 function App() {
   return (
     <div className="App">
+      <Header />
       <main className="App-main">
         <Title />
         <SearchForm />
