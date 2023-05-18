c68e8f6f62bee929cd03bff30f338931d5f78353
Augustine-edeh <info.augustinesedeh@gmail.com>
2023-05-18 18:15:35 +0100

    Modularize BackButton Component Closes #102

---
+++ src/Components/BackButton.js
@@ -0,0 +1,10 @@
+import "./BackButton.css";
+
+const BackButton = () => {
+  return (
+    <button className="BackButton">
+      <span>&larr;</span> <span>Back</span>
+    </button>
+  );
+};
+export default BackButton;
