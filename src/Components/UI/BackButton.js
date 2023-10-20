3f45b91a16dc375dedf156fdec704ced5de50404
Augustine-edeh <info.augustinesedeh@gmail.com>
2023-10-20 01:52:18 +0100

    Atempt style fix

--- src/Components/UI/BackButton.js
+++ src/Components/UI/BackButton.js
@@ -1,13 +1,59 @@
-import { Link } from "react-router-dom";
+// import React, { useRef } from "react";
+// import { Link, useNavigate } from "react-router-dom";
+// import "./BackButton.css";
+
+// const BackButton = () => {
+//   const navigate = useNavigate();
+//   const containerRef = useRef(null);
+
+//   const goToHome = () => navigate("/GoCountry/");
+//   const handleHoverAndFocus = () => {
+//     containerRef.current.focus();
+//   };
+
+//   return (
+//     <div className="BackButton-container">
+//       <button
+//         className="BackButton"
+//         onClick={goToHome}
+//         onMouseEnter={handleHoverAndFocus}
+//         onFocus={handleHoverAndFocus}
+//       >
+//         <span>&larr;</span> <span>Back</span>
+//       </button>
+//     </div>
+//   );
+// };
+// export default BackButton;
+
+import React, { useRef } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "./BackButton.css";
 
 const BackButton = () => {
+  const navigate = useNavigate();
+  const containerRef = useRef(null);
+
+  const goToHome = () => {
+    navigate("/GoCountry/");
+  };
+
+  const handleHoverAndFocus = () => {
+    containerRef.current.focus();
+  };
+
   return (
-    <Link to="/GoCountry" className="BackButton-container">
-      <button className="BackButton">
+    <div className="BackButton-container" ref={containerRef} tabIndex={-1}>
+      <button
+        className="BackButton"
+        onClick={goToHome}
+        onMouseEnter={handleHoverAndFocus}
+        onFocus={handleHoverAndFocus}
+      >
         <span>&larr;</span> <span>Back</span>
       </button>
-    </Link>
+    </div>
   );
 };
+
 export default BackButton;
